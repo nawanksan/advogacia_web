7fc96b520b82f3c786ee0ea71ab8ddf51fd12372
kawan <devbackgruposna@gmail.com>
2025-11-26 15:44:30 -0300

    adicionado gitignore

--- web_api/settings.py
+++ web_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'internal_api.modules.core.users'
+    'internal_api.modules.core.address'
 ]
 
 MIDDLEWARE = [
