a63dd01322cafac4c2cb73ac8a43c90625049ade
kawan <devbackgruposna@gmail.com>
2025-11-27 16:21:52 -0300

    dockercomposer

--- web_api/settings.py
+++ web_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,9 +82,13 @@ WSGI_APPLICATION = 'web_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DJANGO_DB_NAME", "advogacia"),
+        "USER": os.environ.get("DJANGO_DB_USER", "admin"),
+        "PASSWORD": os.environ.get("DJANGO_DB_PASSWORD", "admin123"),
+        "HOST": os.environ.get("DJANGO_DB_HOST", "localhost"),
+        "PORT": "5432",
     }
 }
 
