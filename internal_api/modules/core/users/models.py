0dc1b70c14559b859dfdf941d4eb48768f2928cb
kawan <devbackgruposna@gmail.com>
2025-11-27 11:35:17 -0300

    implementando o models do users

--- internal_api/modules/core/users/models.py
+++ internal_api/modules/core/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Users(models.Model):
+    cpf: models.CharField = models.CharField(
+        verbose_name=_('CPF'),
+        max_length=11
+    )
+    full_name: models.CharField = models.CharField(
+        verbose_name=_('Full Name'),
+        max_length=70
+    )
+    email: models.CharField = models.CharField(
+        verbose_name=_('Email')
+    )
