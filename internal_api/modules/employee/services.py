69f3722edfcedd66815c3738e68442fa52e380c9
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2025-11-29 18:08:52 -0300

    test

--- internal_api/modules/employee/services.py
+++ internal_api/modules/employee/services.py
@@ -5,5 +5,7 @@ from internal_api.modules.employee.repositories import EmployeeRepository
 
 
 class EmployeeService(Service):
-
+    """
+    Camada responsável pelas regras de negócio
+    """
     repository = EmployeeRepository
