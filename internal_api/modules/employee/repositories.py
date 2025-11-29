5a7a69426546840bfa5bcbad063b8b5fea6e17e5
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2025-11-29 18:14:57 -0300

    docs(employee): test 3 (👤: kawan nascimento)

--- internal_api/modules/employee/repositories.py
+++ internal_api/modules/employee/repositories.py
@@ -5,5 +5,8 @@ from internal_api.modules.employee.models import Employee
 
 
 class EmployeeRepository(Repository):
+    """
+    Camada responsável pela comunicação com o banco
+    """
 
     model = Employee
