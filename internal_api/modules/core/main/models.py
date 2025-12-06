d01745359f1abc321304b26048cd27f212d75e0b
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2025-12-06 13:52:13 -0300

    feat(address): implements new fields in class abstractbasemodel (👤: kawan nascimento)

--- internal_api/modules/core/main/models.py
+++ internal_api/modules/core/main/models.py
@@ -9,6 +9,15 @@ class AbstractBaseModel(models.Model):
         verbose_name=_('Active'),
         default=True,
     )
+    last_modification: models.DateTimeField =  models.DateTimeField(
+        verbose_name=_("Last Modification"),
+        auto_now=True,
+    )
+    registration: models.DateTimeField = models.DateTimeField(
+        verbose_name=_("Registration Date"),
+        auto_now_add=True,
+    )
+    # last_user
 
     class Meta:
         abstract = True
